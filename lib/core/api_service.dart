5f330a2ef85606bd7c3a2eb6fcd92c56ba49a846
Gabriel Kofi Owusu <59026711+GabbyOwusu@users.noreply.github.com>
2025-09-10 22:11:27 +0000

    setup api service

--- lib/core/api_service.dart
+++ lib/core/api_service.dart
@@ -1,1 +1,12 @@
-abstract class ApiService {}
+abstract class ApiService {
+  void init({
+    Duration connectTimeout = const Duration(seconds: 30),
+    Duration receiveTimeout = const Duration(seconds: 30),
+    Duration sendTimeout = const Duration(seconds: 30),
+    bool shouldRetryRequest = true,
+    int maxRetries = 3,
+    Duration initialRetryDelay = const Duration(milliseconds: 500),
+    double retryMultiplier = 2.0,
+    Duration maxRetryDelay = const Duration(seconds: 30),
+  });
+}
--- lib/core/api_service_impl.dart
+++ lib/core/api_service_impl.dart
@@ -1,3 +1,63 @@
-import 'package:mini_app/core/api_serivce.dart';
+import 'package:mini_app/core/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:mini_app/core/interceptors.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:mini_app/utils/constants.dart';
 
-class ApiServiceImpl extends ApiService {}
+class ApiServiceImpl extends ApiService {
+  late Dio _dio;
+
+  @override
+  void init({
+    Duration connectTimeout = AppConstants.connectTimeout,
+    Duration receiveTimeout = AppConstants.receiveTimeout,
+    Duration sendTimeout = AppConstants.sendTimeout,
+    bool shouldRetryRequest = true,
+    int maxRetries = AppConstants.maxRetries,
+    Duration initialRetryDelay = AppConstants.initialRetryDelay,
+    double retryMultiplier = AppConstants.retryMultiplier,
+    Duration maxRetryDelay = AppConstants.maxRetryDelay,
+  }) {
+    const baseUrl = AppConstants.baseUrl;
+    final baseOptions = BaseOptions(
+      baseUrl: baseUrl,
+      connectTimeout: connectTimeout,
+      receiveTimeout: receiveTimeout,
+      sendTimeout: sendTimeout,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+
+    _dio = Dio(baseOptions);
+
+    if (shouldRetryRequest) {
+      _dio.interceptors.add(RetryInterceptor(
+        maxRetries: maxRetries,
+        initialDelay: initialRetryDelay,
+        multiplier: retryMultiplier,
+        maxDelay: maxRetryDelay,
+      ));
+    }
+
+    _dio.interceptors.add(AuthInterceptor());
+
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final String jsonString = await rootBundle.loadString('db.json');
+          final List<dynamic> jsonData = json.decode(jsonString);
+          return handler.resolve(
+            Response(
+              requestOptions: options,
+              data: jsonData,
+              statusCode: 200,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
